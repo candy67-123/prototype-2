76583b984ff8c2c2e963f5bbcdce31196d90173e
candy67-123 <zizhao4@s.sfusd.edu>
2023-01-25 10:31:51 -0800

    Launch projectile on spacebar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,20 @@ public class PlayerController : MonoBehaviour
     public float xRange = 10;
     public float horizontalInput;
     public float speed = 10.0f;
+
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         // Keep the player in bounds
-        if (transform.position.x < - xRange)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
@@ -28,5 +31,15 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
+
+
+    
+
+
